9a83fb4f3e13207aa94855a3d9b86a94f041f870
venu <vg37@zips.uakron.edu>
2015-10-10 15:43:04 -0400

    ui screen

---
+++ AutoInsurance/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoInsurance
+//
+//  Created by Gangisetty,Venugopal on 10/10/15.
+//  Copyright © 2015 Gangisetty,Venugopal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
